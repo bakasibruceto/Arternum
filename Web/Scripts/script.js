669a2c240c8353245f672c1bf7cb4fb00ae3cc14
bakasibruceto <abla09027@gmail.com>
2023-04-20 20:27:25 +0800

    MainStyle

--- Web/Scripts/script.js
+++ Web/Scripts/script.js
@@ -4,9 +4,11 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("navbar").style.padding = "1px";
-    document.getElementById("logo").style.fontSize = "20px";
+    document.getElementById("logo").style.fontSize = "25px";
+    document.getElementById("navbar").style.paddingLeft = "90px";
   } else {
     document.getElementById("navbar").style.padding = "28px";
     document.getElementById("logo").style.fontSize = "35px";
+    document.getElementById("navbar").style.paddingLeft = "100px";
   }
 }
