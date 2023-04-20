fa428f940eb5a41ab478cd512b7d3779d3cfa1b9
bakasibruceto <abla09027@gmail.com>
2023-04-21 01:57:58 +0800

    updated

--- Web/Scripts/script.js
+++ Web/Scripts/script.js
@@ -5,22 +5,29 @@ function scrollFunction() {
   
 
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    
     document.getElementById("avatar").style.height = "40px";
     document.getElementById("avatar").style.width = "40px";
+    
     document.getElementById("wrapper").style.padding = "5px";
-    document.getElementById("navbar").style.paddingBottom = "100px";
+    document.getElementById("wrapper2").style.padding = "29px";
     document.getElementById("wrapper").style.paddingRight = "160px";
-    document.getElementById("logo").style.fontSize = "28px";
+
     
   } else {
     
 
     document.getElementById("avatar").style.height = "60px";
     document.getElementById("avatar").style.width = "60px";
-    document.getElementById("wrapper").style.padding = "15px";
+  
+    document.getElementById("wrapper2").style.padding = "50px";
+
+    document.getElementById("wrapper").style.paddingBottom = "100px";
+    document.getElementById("wrapper").style.paddingTop = "15px";
+    document.getElementById("wrapper2").style.paddingTop = "39px";
     document.getElementById("wrapper").style.paddingRight = "165px";
-    document.getElementById("navbar").style.paddingLeft = "100px";
-    document.getElementById("logo").style.fontSize = "35px";
+
+  
    
   }
 }
