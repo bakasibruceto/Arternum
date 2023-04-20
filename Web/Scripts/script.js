c463d3d80f49d3949db49cab83ac5a789fdc7762
bakasibruceto <abla09027@gmail.com>
2023-04-21 04:07:24 +0800

    update

--- Web/Scripts/script.js
+++ Web/Scripts/script.js
@@ -9,19 +9,20 @@ function scrollFunction() {
     //ProfileIcon
     document.getElementById("avatar").style.height = "40px";
     document.getElementById("avatar").style.width = "40px";
+    //document.getElementById("search").style.height = "40px";
+    //document.getElementById("search").style.width = "40px";
+    
     //GreyBar
     document.getElementById("wrapper").style.padding = "10px 10px 50px 10px";
 
- 
-
-    
   } else {
     
     //ProfileIcon
     document.getElementById("avatar").style.height = "60px";
     document.getElementById("avatar").style.width = "60px";
+    //document.getElementById("search").style.height = "60px";
+    //document.getElementById("search").style.width = "60px";
     
-    document.getElementById("profile").style.paddingRight = "10%";
     //GreyBar
     document.getElementById("wrapper").style.padding = "10px 10px 70px 10px";
   
