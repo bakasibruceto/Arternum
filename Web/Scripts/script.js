6c4980e481cc3148c760dd7363a6310105676046
bakasibruceto <abla09027@gmail.com>
2023-04-21 04:39:02 +0800

    updated

--- Web/Scripts/script.js
+++ Web/Scripts/script.js
@@ -9,8 +9,8 @@ function scrollFunction() {
     //ProfileIcon
     document.getElementById("avatar").style.height = "40px";
     document.getElementById("avatar").style.width = "40px";
-    //document.getElementById("search").style.height = "40px";
-    //document.getElementById("search").style.width = "40px";
+    document.getElementById("search").style.height = "40px";
+    document.getElementById("search").style.width = "40px";
     
     //GreyBar
     document.getElementById("wrapper").style.padding = "10px 10px 50px 10px";
@@ -20,8 +20,8 @@ function scrollFunction() {
     //ProfileIcon
     document.getElementById("avatar").style.height = "60px";
     document.getElementById("avatar").style.width = "60px";
-    //document.getElementById("search").style.height = "60px";
-    //document.getElementById("search").style.width = "60px";
+    document.getElementById("search").style.height = "60px";
+    document.getElementById("search").style.width = "60px";
     
     //GreyBar
     document.getElementById("wrapper").style.padding = "10px 10px 70px 10px";
