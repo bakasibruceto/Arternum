04c886b70b4e539326f06f8c6db6824432493c65
bakasibruceto <abla09027@gmail.com>
2023-04-21 15:34:27 +0800

    updated

--- Web/JS/navbar.js
+++ Web/JS/navbar.js
@@ -1,45 +1,50 @@
 //Navbar on scroll
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-  
 
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    
+
     //ProfileIcon
     document.getElementById("avatar").style.height = "40px";
     document.getElementById("avatar").style.width = "40px";
-    
+
+    //LogoIcon
     document.getElementById("logo").style.height = "40px";
     document.getElementById("logo").style.width = "40px";
+
+    //Search Icon
     document.getElementById("search").style.marginTop = "1%";
+
+    //Text
     document.getElementById("navbar").style.marginTop = ".9%";
     document.getElementById("navbar").style.marginLeft = "16%";
-    
-  
-  
-    //GreyBar
+
+    //PurpleWrapp
+    document.getElementById("wrapper").style.opacity = "1";
     document.getElementById("wrapper").style.padding = "10px 10px 50px 10px";
 
   } else {
-    
+
     //ProfileIcon
     document.getElementById("avatar").style.height = "60px";
     document.getElementById("avatar").style.width = "60px";
+
+    //Search Icon
     document.getElementById("search").style.marginTop = "2%";
+
+    //Text
     document.getElementById("navbar").style.marginTop = "2%";
     document.getElementById("navbar").style.marginLeft = "17%";
+
+    //LogoIcon
     document.getElementById("logo").style.height = "60px";
     document.getElementById("logo").style.width = "60px";
 
-    
-    //GreyBar
-    document.getElementById("wrapper").style.padding = "10px 10px 80px 10px";
-  
-  
- 
 
-  
-   
+    //NavBar
+    document.getElementById("wrapper").style.opacity = "0";
+    document.getElementById("wrapper").style.padding = "10px 10px 80px 10px";
+
   }
 }
