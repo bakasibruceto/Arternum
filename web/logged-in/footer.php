7546384a20cb10669eafb8cf7daee32948d4419d
bakasibruceto <abla09027@gmail.com>
2023-04-23 16:19:40 +0800

    Organized Files

---
+++ web/logged-in/footer.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<div id="footer">
+    <a href="">Terms</a>
+    <a href="">About-us</a>
+    <a href="">References</a>
+    <a href="https://github.com/bakasibruceto/2nd-Year-WebDev-Finals-Project-2nd-Sem-2023" target="_blank">Source
+      Code</a>
+    <br>
+    <h5>powerd by aternum</h5>
+  </div>
+</body>
+</html>
---
+++ web/logged-in/header2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div id="header2">
+    <ul>
+      <li><a href="#">dashboard</a></li>
+      <li><a href="#">friends</a></li>
+      <li><a href="#">account settings</a></li>
+    </ul>
+  </div>
+</body>
+</html>
