d6440b9f4de6584aac61a3c004974068a3b7ccfa
bakasibruceto <abla09027@gmail.com>
2023-04-24 17:08:55 +0800

    header2

--- PHP/pages/logged-in/header2.php
+++ PHP/pages/logged-in/header2.php
@@ -1,18 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
 </head>
+
 <body>
-<div id="header2">
-    <ul>
-      <li><a href="#">dashboard</a></li>
-      <li><a href="#">friends</a></li>
-      <li><a href="#">account settings</a></li>
-    </ul>
+  <div id="header2">
+    <div id="hov"></div>
+    <form action="#" method="GET">
+      <ul>
+        <li><a href="#" id="no">dashboard</a></li>
+        <li><a id="t">&nbsp;&nbsp;</a></li>
+        <li><a href="" id="no" type="submit" name="friends" >friends</a></li>
+        <li><a id="t">&nbsp;&nbsp;</a></li>
+        <li><a href="#" id="no">account settings</a></li>
+      </ul>
+
+    </form>
+
   </div>
 </body>
+
 </html>
