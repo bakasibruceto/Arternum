183d2c371d570b9a130d13f2b13d21844cb7bd42
bakasibruceto <abla09027@gmail.com>
2023-04-24 19:12:59 +0800

    dashboard page

---
+++ index-dashboard.php
@@ -0,0 +1,28 @@
+<?php include('PHP/backend/server.php'); ?>
+<?php
+# cant access page without loging in
+if (!isset($_SESSION["username"])) {
+  header("location: index.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="CSS/logged.css">
+  <script src="JS/script.js"></script>
+</head>
+
+<body>
+  <?php include('PHP/pages/logged-in/navbar.php'); ?>
+  <?php include('PHP/pages/logged-in/header.php'); ?>
+  <?php include('PHP/pages/logged-in/header2.php'); ?>
+  <?php include($content) ?>
+  <?php include('PHP/pages/logged-in/footer.php'); ?>
+
+</body>
+
+</html>
