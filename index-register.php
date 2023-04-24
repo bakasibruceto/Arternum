78d067fee5329e4542481b1cfcad6764296909c1
bakasibruceto <abla09027@gmail.com>
2023-04-24 17:54:00 +0800

    renamed

---
+++ index-register.php
@@ -0,0 +1,47 @@
+<?php include('PHP/backend/server.php') ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title></title>
+</head>
+
+<body>
+    <div id="container">
+        <h1>Register</h1>
+        <br><br><br><br>
+        <form actions="register.php" method="POST">
+            <?php include('PHP/backend/errors.php') ?>
+            <div>
+                <label>username</label>
+                <input type="text" name="username" required>
+            </div>
+            <br><br>
+            <div>
+                <label>email</label>
+                <input type="email" name="email" value="<?php echo $email; ?>">
+            </div>
+            <br><br>
+            <div>
+                <label>password</label>
+                <input type="password" name="password_1" required>
+            </div>
+            <br><br>
+            <div>
+                <label>confirm password</label>
+                <input type="password" name="password_2" required>
+            </div>
+            <br><br>
+            <div>
+                <button type="submit" class="btn" name="register">register</button>
+            </div>
+            <br><br>
+            <div>
+                <a href="index.php"><input type="button" value="Login"></a>
+            </div>
+        </form>
+    </div>
+
+</body>
+
+</html>
