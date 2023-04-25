9d411d3a7f3717628d084c83117c613a2dfa35b7
bakasibruceto <abla09027@gmail.com>
2023-04-25 16:46:14 +0800

    changed to switcher format

--- PHP/pages/dashboard/content/friends.php
+++ PHP/pages/dashboard/content/friends.php
@@ -1,18 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-
-<body>
-  <div id="content">
+<div id="content">
   <h1>FRIENDS</h1>
-
- 
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque voluptatibus recusandae eius odit laboriosam
       quam.
       Asperiores, quos dolor nam laboriosam distinctio vel, sapiente blanditiis maiores aut accusamus quaerat,
@@ -37,9 +24,5 @@
       quam.
       Asperiores, quos dolor nam laboriosam distinctio vel, sapiente blanditiis maiores aut accusamus quaerat,
       suscipit
-      hic.</p>
-      <br><br><br><br><br><br><br><br> <br><br><br><br><br><br><br><br> <br><br><br><br><br><br><br><br>
+      hic.</p>  
   </div>
-</body>
-
-</html>
--- PHP/pages/dashboard/header2.php
+++ PHP/pages/dashboard/header2.php
@@ -9,18 +9,18 @@
 </head>
 
 <body>
-  <div id="header2">
+  <nav>
     <div id="hov"></div>
 
     <ul>
-      <li><a href="index-dashboard.php" id="no">dashboard</a></li>
-      <li><a id="t">&nbsp;&nbsp;</a></li>
-      <li><a href="" id="no" type="submit" name="friends">friends</a></li>
-      <li><a id="t">&nbsp;&nbsp;</a></li>
-      <li><a href="index-account-settings.php" id="no">account settings</a></li>
+      <li><a data-switcher data-tab="1" class="tab is-active">dashboard</a></li>
+      <li><a  id="t">&nbsp;&nbsp;</a></li>
+      <li><a data-switcher data-tab="2" class="tab"  >friends</a></li>
+      <li><a  id="t">&nbsp;&nbsp;</a></li>
+      <li><a  data-switcher data-tab="3"class="tab" >account settings</a></li>
     </ul>
 
-  </div>
+  <nav>
 </body>
 
 </html>
