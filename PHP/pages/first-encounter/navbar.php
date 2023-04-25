d7a5868b216f8d0eae72a2955d36ae6dba8209c0
bakasibruceto <abla09027@gmail.com>
2023-04-25 19:17:19 +0800

    -

---
+++ PHP/pages/first-encounter/navbar.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+  <div id="action">
+    <div id="drop">
+      <div id="menu">
+        <form actions="#" method="POST" autocomplete="off">
+        
+          <p>Sign in To Proceed</p>
+          <br>
+          <input type="text" name="username" placeholder="username" required>
+          <br><br>
+          <input type="password" name="password" placeholder="password" required>
+          <br>
+          <!--error.php-->
+          <?php include('PHP/backend/errors.php'); ?>
+          <a href="#">I've forgotten my details</a>
+          <input type="submit" name="login" value="Login" onblur="checkUser(this)">
+        </form>
+
+      </div>
+    </div>
+    <div id="nav">
+      <ul>
+        <li><a href="#">home</a></li>
+        <li><a href="#">about us</a></li>
+      </ul>
+    </div>
+    <div id="sign" onclick="menuToggle()">
+      <ul>
+        <li>
+          <p>sign-in
+          <p>
+        </li>
+      </ul>
+    </div>
+  </div>
+</body>
+
+</html>
---
+++ index-admin-dashboard.php
@@ -0,0 +1,27 @@
+<?php include('PHP/backend/server.php'); ?>
+<?php
+# cant access page without loging in
+if (!isset($_SESSION["username"])) {
+  header("location: index.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="CSS/logged.css">
+  <script src="JS/script.js"></script>
+</head>
+
+<body>
+  <?php include('PHP/pages/admin/dashboard/navbar.php'); ?>
+  <?php include('PHP/pages/admin/dashboard/header.php'); ?>
+  <?php include('PHP/pages/admin/dashboard/header-dashboard.php'); ?>
+  <?php include('PHP/pages/admin/dashboard/content/home.php') ?>
+  <?php include('PHP/pages/admin/dashboard/footer.php'); ?>
+</body>
+
+</html>
