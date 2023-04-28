487955e8696ebe682d1b755f234e0c3538bbcc6a
bakasibruceto <abla09027@gmail.com>
2023-04-28 08:57:31 +0800

    account settings ui

--- index-account-settings.php
+++ index-account-settings.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION["username"])) {
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Admin</title>
-  <link rel="stylesheet" href="CSS/account-settings.css">
+  <link rel="stylesheet" href="CSS/logged2.css">
   <script src="JS/script.js"></script>
 </head>
 <body>
